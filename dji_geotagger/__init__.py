7ce91570edebfe3a07292eba0733b3b8f40ba436
RayPan-UC <hungwei.pan2@ucalgary.ca>
2026-02-27 18:42:39 -0700

    remove: clean temp file function

--- dji_geotagger/__init__.py
+++ dji_geotagger/__init__.py
@@ -1,4 +1,4 @@
 from dji_geotagger.ppk.raw_converter import raw2rinex
 from dji_geotagger.ppk.ppk_solver import process_ppk
 from dji_geotagger.core.camera_pos_solver import load_and_compute_camera_positions
-from dji_geotagger.tools.tools import transform_coordinates, get_crs_igb20, clean_temp_dirs, pause_for_PPP_sum_file, save_csv
+from dji_geotagger.tools.tools import transform_coordinates, get_crs_igb20, pause_for_PPP_sum_file, save_csv
